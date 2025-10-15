08a842d94d0a8d321fc4178e34d4b3b5103295fa
lucasbalves <lkab05@hotmail.com>
2025-10-15 12:15:27 -0300

    refactor: convert ContaBancaria from record to class with explicit fields and methods

--- src/model/ContaBancaria.java
+++ src/model/ContaBancaria.java
@@ -5,7 +5,25 @@ import java.util.List;
 
 import model.types.TipoConta;
 
-public record ContaBancaria(String numero, String agencia, Cliente cliente, BigDecimal saldo, BigDecimal limite, TipoConta tipoConta, List<Transacao> historico) {
+public class ContaBancaria {
+    private String numero;
+    private String agencia;
+    private Cliente cliente;
+    private BigDecimal saldo;
+    private BigDecimal limite;
+    private TipoConta tipoConta;
+    private List<Transacao> historico;
+
+    public ContaBancaria(String numero, String agencia, Cliente cliente, BigDecimal saldo, BigDecimal limite, TipoConta tipoConta, List<Transacao> historico) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.cliente = cliente;
+        this.saldo = saldo;
+        this.limite = limite;
+        this.tipoConta = tipoConta;
+        this.historico = historico;
+    }
+
     public static ContaBancaria newContaBancaria(String numero, String agencia, Cliente cliente, BigDecimal saldo, BigDecimal limite, TipoConta tipoConta, List<Transacao> historico) {
         return new ContaBancaria(numero, agencia, cliente, saldo, limite, tipoConta, historico);
     }
@@ -22,7 +40,66 @@ public record ContaBancaria(String numero, String agencia, Cliente cliente, BigD
         return numero + "-" + agencia;
     }
 
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public BigDecimal getLimite() {
+        return limite;
+    }
+
+    public void setLimite(BigDecimal limite) {
+        this.limite = limite;
+    }
+
+    public TipoConta getTipoConta() {
+        return tipoConta;
+    }
+
+    public List<Transacao> getHistorico() {
+        return historico;
+    }
+
+    
+
+    @Override
     public String toString() {
         return String.format("ContaBancaria[numero=%s, agencia=%s, cliente=%s, saldo=%s, limite=%s, tipoConta=%s]", numero, agencia, cliente, saldo, limite, tipoConta.getDescricao());
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setTipoConta(TipoConta tipoConta) {
+        this.tipoConta = tipoConta;
+    }
+
+    public void setHistorico(List<Transacao> historico) {
+        this.historico = historico;
     }
 }
